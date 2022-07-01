7fd0e9f94ae956d6ee86567b58a2067f6b5c29ea
Joao Garduño <joao.garduno@gmail.com>
2022-07-01 11:08:44 -0500

    Tema 1 y 2 -> Comentarios y Tipos de datos

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// TEMA 1: Comentarios
+
+// los comentarios se ignoran
+
+// Comentario de una sola linea
+
+/* Comentario de 
+Varias lineas */
+// ------------------------------------------------------
+
+
+// TEMA 2: Tipo de datos
+
+// En JavaScript se trabajan con valores, y cada uno de estos valore tienen un tipo especifico de dato cada uno de ellos dependiendo de su proposito dentro del codigo.
+
+// Incluyendo:
+    // undefined -> no exite valor asignado
+    // null --> Valor nulo, es decir no existe
+    // boolean --> Verdadero y falso
+    // string --> cadena de caracteres.
+    // symbol -->
+    // number --> representa un numero en nuestro codigo
+    // object --> respresenta un objeto
+
+// Objeto: es la estructura que nos va a permitir relacionar:
+    // Propiedades
+    // Funciones
+// Bajo una misma estructura que llamamos objeto
+// ------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------
+// ------------------------------------------------------
+// ------------------------------------------------------
+// ------------------------------------------------------
