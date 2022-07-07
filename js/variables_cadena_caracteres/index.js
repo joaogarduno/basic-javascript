4bc8efc8055832a09c3109e2c0db897bfe1f14da
Joao Garduño <joao.garduno@gmail.com>
2022-07-07 10:27:36 -0500

    Tema 19: Variables con cadena de caracteres

---
+++ js/variables_cadena_caracteres/index.js
@@ -0,0 +1,42 @@
+// TEMA 19: Variables con cadenas de caracteres
+
+// Usamos cadena de caracteres para representar texto en nuestro codigo, por ejemplo si queremos representar un nombre podemos crear una cadena de caracteres
+// y podemos asignarle una variable para poder usarla en nuestro programa
+var nombre = 'Pedro';
+
+// La cadena de caracteres se definen o se crean en el programa rodeandolas con comillas ("")
+// Estas pueden ser "comillas dobles" o "comillas simples" ambas funcionan 
+
+// existe una cierta restrigcion en usar cada una de estas.
+
+// Entonces todos los caracteres que se encuentran en medio de las comillas son considerados parte de la cadena de caracteres, incluyendo:
+    // los espacios -> tienen un lugar especifico en la cadena y se considera como otro caracter.
+    // cualquier simbolo -> cualquiera que podamos escribir con el teclado tambien se considera un caracter en la cadena.
+    // cualquier letra
+    // cualquier numero
+    // cualquier simbolo -> por ejemplo un guion bajo (_)
+
+// Los operadores pierden su poder cuando estan entre comillas por lo que se comportan diferente y no ejecutan una operacion solamente representan los simbolos que son
+
+var apellido = 'Saldaña Medrana';
+
+
+// Al momento de usar las comillas dobles y simples, es que seas consistente no puedes comenzar con una comilla simple y terminar con una comilla doble porque tendremos un error
+
+// var apellido = 'macarena ferrano";
+// Esto es un error
+
+
+
+
+// ----------------
+// ----------------
+// ----------------
+
+
+
+
+// ------------------------------------------------------
+// ------------------------------------------------------
+// ------------------------------------------------------
+// ------------------------------------------------------
