20ea61f98f5b2b90c1bd0ecb303b4717197b43dd
Joao Garduño <joao.garduno@gmail.com>
2022-07-14 22:16:30 -0500

    Tema 33: Arreglos (Arrays) ->

---
+++ js/02_Arreglos_Arrays/index.js
@@ -0,0 +1,60 @@
+// TEMA 33: AREGLOS O ARRAYS
+
+// Veremos otro tipo de dato muy importante en JavaScript
+
+// Lo Arreglos son estructuras de datos que nos permiten almacenar multiples valores en una misma estructura.
+
+// Son muy parecidos a los que nosotros considerariamos una lista
+// ----------------
+
+
+
+
+// Vamos a definir una variable que se llama "arreglo" y a esta le asignamos un arreglo
+// Para crear un arreglo escribimos corchetes y dentro de los corchetes tendriamos los elementos de ese arreglo
+
+// Por ejemplo:
+    // "john" y 24 lo cual representan una cadena de caracteres y un numero.
+
+// Los elementos de un arreglo estan en una secuencia especifica, cada uno tiene su lugar en esa secuencia y la secuencia esta en orden especifico
+
+//  Asi como podiamos acceder a los caracteres de una cadena con sus indices tambien vamos a poder acceder a cada uno de estos elementos con su indice correspondiente.
+
+var miArreglo = ['John', 24];
+console.log(miArreglo);
+
+// La ventaja de los arreglos es que podemos agrupar valores bajo una misma estructura
+// ----------------
+
+
+
+
+// OTRO EJEMPLO
+var estudiantes = ['Nora', 'Dino', 'Arldnold', 'Patricio', 'Arenita'];
+console.log(estudiantes);
+// ----------------
+
+
+// OTRO EJEMPLO
+var notas = [95, 89, 67, 56, 90, 100];
+console.log(notas);
+
+
+// Entonces podemos entender que los arreglos pueden ser muy poderosos para trabajar con valores relacionados, en este caso tenemos "nombres de estudiantes", "notas", "persona"
+// ------------------------------------------------------------------
+
+
+
+
+
+
+
+// ----------------
+// ----------------
+// ----------------
+
+
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
