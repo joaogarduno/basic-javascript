4b426d532f11880f960899a935795a06ca880d3c
Joao Garduño <joao.garduno@gmail.com>
2022-07-19 09:33:05 -0500

    Tema 43: Funciones

---
+++ js/03_funciones/index.js
@@ -0,0 +1,47 @@
+// TEMA 43: Funciones
+
+// Para estructurar mejor nuestros programas vamos a ver un concepto una herramienta que podemos usar en nuestro codigo que es muy util y se llaman "funciones"
+
+// Con las funciones podemos escribir codigo que podemos reutilizar en nuestro programa.
+
+// ¿Como definimos una funcion?
+function nombreDeNuestraFuncion(){
+    console.log("Hola, esta es una funcion!!");
+}
+// Esto es una definicion de una funcion.
+
+// ¿Que diferecia va hacer esto en nuestro programa?
+// Lo que vamos a poder hacer es que, solamente con este nombre de "nombreDeNuestraFuncion" vamos a poder ejecutar la linea de codigo que esta dentro que dice asi "console.log("Hola, esta es una funcion!!");" sin tener que escribirla y queremos realizar esa accion y podemos usarla cuantas veces queramos.
+
+// ejecutamos la funcion anterior
+nombreDeNuestraFuncion();
+// Cuando ejecutamos una funcion decimos que estamos llamando a la funcion.
+
+// Esto nos va a permitir ahorrar muchas repeticion de codigo, el cual es un principio muy importante en la programacion
+
+// Recuerda que siempre tratamos de no repetir codigo en nuestro programa, lo menos posible.
+// ------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------
+// ----------------
+// ----------------
+
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
