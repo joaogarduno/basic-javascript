c02b430ccb46a877e02dc7f02ad73c106c12982f
Joao Garduño <joao.garduno@gmail.com>
2022-07-21 09:28:04 -0500

    Tema 52: Valores Booleanos

---
+++ js/05_valores_y_operadores/index.js
@@ -0,0 +1,62 @@
+// TEMA 52: Valores Booleanos
+
+// Hablaremos de otro tipo de datos con el cual podemos trabajar en javascript, y es el tipo de datos de "valores booleanos"
+
+// En JavaScript formalmente ese tipo de datos se denomina "booleans"
+// Esos valores booleanos en JavaScript son "Verdadero" y "Falso", que en el lenguaje de programacion se escribe o se conocer como "TRUE" y "FALSE".
+
+// Estos son los dos valores booleanos (TRUE y FALSE) con los cuales podemos trabajar en JavaScript, pero es muy importante que conozcamos como poder escribirlos para que se reconozcan como esos valores especiales en el lenguaje, y estas es la unica forma de escribirlos formalmente  en el lenguaje para que se reconozcan como esos valores especiales en el lenguaje.
+
+// Esta es la unica forma de escribirlos formalmente en el lenguaje para que se reconozcan con sus capacidades especiales de valores de verdad.
+
+// Por ejemplo si nosotros queremos demostrar el valor TRUE, podemos ver que cuando lo escribimos en el codigo su color cambia, porque es un valor especial en el lenguaje.
+console.log(true);
+
+// PEro si lo escribimos en nuestro lenguaje con una letra mayuscula al inicio, notamos que ya el color no es el mismo
+console.log(True);
+
+// Veamos en consola que sucede con estas 2 forma, y notamos que el primer TRUE si es correcto y vemos como resultado en consola TRUE, pero en el segundo, notamos un error, y nos dice que "True" no esta definido ¿Por qué? porque como "True" no esta definido no lo reconoce, es decir, no esta escrito correctamente con una "T" mayuscula y el lenguaje no lo reconocer como una palabra especial en el lenguaje.
+
+// Tambien si lo escribimos con cualquier otra convinacion de mayusculas o minusculas, podemos ver que no va a funcionar.
+console.log(TRUE);
+console.log(TrUe);
+console.log(tRue);
+// ----------------
+
+
+
+// Lo mismo ocurre con el valor FALSE, tiene que estar escrito completamente en minuscula.
+console.log(false);
+
+// cualquier otra convinacion nos va a dar error.
+console.log(FALSE);
+console.log(False);
+
+// Entonces la unica forma de escribir FALSE como el valor Booleano y se interprete como el valor correcto que podemos usar en nuestro programa, es escribir "true" y "false" completamente en minuscula.
+
+// Estos valores "true" y "false" van a ser muy utiles cuando empecemos a escribir condicionales, cuando empecemos a realizar comparaciones de valores y cuando trabajemos con operadores logicos.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------
+// ----------------
+// ----------------
+
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
