3a9434825b54f15bc96beea859765219582cb703
Joao Garduño <joao.garduno@gmail.com>
2022-08-23 21:35:14 -0500

    Tema 80: Acceder a Propiedades: Notación de Corchetes

--- js/07_Objetos/index.js
+++ js/07_Objetos/index.js
@@ -49,7 +49,64 @@ console.log(miPerro.edad);
 console.log(miPerro.raza);
 
 // Asi es como accedemos a las propiedades de un objeto, recuerda con el nombre de la variable segido de un "punto" y luego el nombre de la "propiedad"
+// ------------------------------------------------------------------
 
+
+
+
+
+// TEMA 80: Acceder a Propiedades: Notación de Corchetes
+
+// Notacion alternativa para acceder a las "propiedades" de un objeto.
+
+// Primero definimos nuestro objeto y vamos asignarlo a la variable "miCuaderno", este objeto tendra las propiedades "color", "categoria", "numero de paginas", "numero de hojas"
+var miCuaderno = {
+    "color": "verde",
+    "categoria": 3,
+    "numero de paginas": 200,
+    "numero de hojas": 100
+};
+// Este es nuestro objeto, y como podemos notar en el nombre de las ultimas 2 "propiedades", "numero de paginas" y "numero de hojas" tenemos espacios, pero como se estan rodeando entre comillas podemos escribirlas, solo podemos omitir las comillas cuando el nombre tenga una sola palabra, si tiene mas de una palabra y hay espacio en ese nombre tenemos que rodearla entre comillas es obligatorio.
+console.log(miCuaderno.color);
+
+// Ahora vamos a utilizar la notación de corchetes, es una notacion que es equivalente, ya que normalmente utilizamos la notacion de punto, pero la "notación de corchetes" nos permite acceder a este tipo de propiedades que tienen "espacios", esas propiedades que tienen "espacios en su nombre", en ese caso es obligatorio, pero tambien podemos utilizarlas para acceder a cualquier propiedad digamos "color"
+console.log(miCuaderno["color"]);
+// ----------------
+
+
+
+
+// OTRO EJEMPLO:
+
+// Cuando utilizamos la notacion de corchetes, tenemos que escribir un par de corchetes y dentro de los corchetes el nombre de la propiedad rodeado por comillas, podemos acceder a cualquier propiedad digamos "numero de paginas"
+var miCuaderno = {
+    "color": "verde",
+    "categoria": 3,
+    "numero de paginas": 200,
+    "numero de hojas": 100
+};
+
+console.log(miCuaderno["numero de paginas"]);
+// Y vemos en consola el valor de "200"
+// ----------------
+
+
+
+
+// OTRO EJEMPLO:
+
+// Con notacion de "punto", esto daria un error, digamos que tratamos de acceder a esta propiedad con la notación de punto y veremos un error, ya que despues del primer espacio no se detecta como un solo nombre, normalmente lo que hacemos en ese caso es unir las palabras comenzando con mayusculas, como los nombres de las variables, en ese caso si podriamos usar "notación de punto" o "notación de corchetes" que es equivalente
+var miCuaderno = {
+    "color": "verde",
+    "categoria": 3,
+    "numeroDePaginas": 200,
+    "numeroDeHojas": 100
+};
+
+console.log(miCuaderno.numeroDeHojas);
+
+// Ahora como la "notacion de punto" es mas concisa y mas facil de leer normalmente utilizamos "notacion de punto", pero la "notacion de corchetes es muy util" tambien cuando necesitamos usar el valor de una variable
+// ------------------------------------------------------------------
 
 
 
