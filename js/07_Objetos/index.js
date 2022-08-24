36a860ebe644c1d526dd3d06af1ef7675441ab16
Joao Garduño <joao.garduno@gmail.com>
2022-08-23 21:03:29 -0500

    Tema 78: Crear Objetos

---
+++ js/07_Objetos/index.js
@@ -0,0 +1,45 @@
+// TEMA 78: Crear Objetos
+
+// Los objetos nos permiten guardar una secuencia o un conjunto de propiedades que estan relacionadas con sus correspondientes valores.
+
+// Por ejemplo, si nosotros queremos describir un perro especifico, una mascota especifica, digamos que queremos representar en nuestro programa las propiedades de nuestro perro, ese perro tiene varias propiedades.
+// Esta combinacion de propiedades, como "nombre", "edad", "peso", "raza" y de valores que estan relacionados a esas propiedades, lo denominamos un objeto, que podemos usar en Javascript en nuestro programa
+// Es como una forma de agruparlas todas bajo una misma estructura de datos, que, podemos usar y acceder a traves de esta variable
+var miPerro = {
+    "nombre": "Peluso",
+    "edad": 5,
+    "peso": 6,
+    "raza": "Beagle"
+};
+// Podemos usar numeros como propiedades e incluso tambien podemos omitir tambien las comillas, si los nombres de las propiedades tienen una sola palabra, no teniendo ningun espacio.
+// ----------------
+
+
+
+
+// Otro ejemplo
+
+// Tambien podemos utilizar numeros como propiedades
+var miObjeto = {
+    5: "cinco"
+};
+// Si el objeto tiene una propiedad que no es una cadena JavaScript las combierte automaticamente en cadenas de texto, asi que si no las rodeamos en comillas de igualforma van a ser comillas en la representacion interna del programa
+// ------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+// ----------------
+// ----------------
+// ----------------
+
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
