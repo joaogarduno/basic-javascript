84c42e9f180f431d1ab7b35951165ca2e06121ee
Joao Garduño <joao.garduno@gmail.com>
2024-09-19 11:44:42 -0600

    Tema 113: Funciones Flecha con Parámetros

--- js/12_funciones_flecha/index.js
+++ js/12_funciones_flecha/index.js
@@ -36,6 +36,63 @@ const fecha = () => new Date();
 
 
 
+// TEMA 113: FUNCIONES FLECHA CON PARÁMETROS
+// 
+// Cómo definir funciones flecha que tomen uno o mas parametros, en el ejemplo anterior la funcion flecha no tomaba ningun parametro solo retornaba un objeto.
+
+// En este ejemplo definiremos una funcion anonima que va a tomar el valor X y retornara el valor X + 3, por eso vamos asignarle a la variable sumar 3, ahora digamos que queremos transformar esta función en una funcion flecha para escribirla de manera mas concisa en el programa.
+
+const sumaTres = (x) => x + 3
+
+// Este es el equivalente en una función flecha
+console.log(sumaTres(4));
+
+
+
+// ----------------
+// OTRO EJEMPLO
+// Ahora vamos a hacer algo distinto, vamos a definir una función anonima la vamos a asignar a la variable concatenar y vamos a tomar 2 parametros "arreglo 1" y "arreglo 2"
+const concatenar = function (arre1, arre2){
+  return arre1.concat(arre2);
+}
+
+// Retornaremos los dos arreglos concatenados o los dos arreglos unidos, los elementos del arreglo 2 se van a añadir al final del arreglo 1 y lo hacemos llamando al método "concat()"
+// Este es un metodo que puedes llamar para unir un arreglo con otro
+console.log(concatenar([1, 2], [3, 4, 5]));
+
+
+
+// ----------------
+// OTRO EJEMPLO
+// Ahora vamos a transformar esta función en una funcion flecha, exactamente igual que vimos en el ejemplo anterior...
+const concatenar = (arre1, arre2) => arre1.concat(arre2);
+
+console.log(concatenar([6, 7], [8, 9, 1]));
+// Estos casos que estamos viendo eran funciones que solo tenian una línea y retornaban un valor, si la función original tienen mas de una línea tenemos que mantener las llaves.
+
+
+
+// ----------------
+// OTRO EJEMPLO
+// Si notros teniamos una función que tomaba 2 parametros "a" y "b"
+const sumar = function(a, b){
+  let num = 6;
+  return a + b + num
+}
+
+
+// En este caso lo que hariamos para poder definir la función flecha seria borrar la palabra y antes de la llave que abre el cuerpo de la función colocamos el simbolo de la fechita
+const sumar = (a, b) => {
+  let num = 6;
+  return a + b + num;
+};
+
+// Y así tenemos la misma función que teniamos antes.
+console.log(sumar(1, 1));
+// ------------------------------------------------------------------
+
+
+
 
 
 
